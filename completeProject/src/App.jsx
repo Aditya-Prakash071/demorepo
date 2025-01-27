38146d9c1ddb7e5501428a048bda68b1294c7898
Aditya-Prakash071 <118518844+Aditya-Prakash071@users.noreply.github.com>
2025-01-27 13:17:21 +0000

    changes done

---
+++ completeProject/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-3xl text-blue-600'>aditya</div>
+    </>
+  )
+}
+
+export default App
