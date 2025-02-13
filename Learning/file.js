580202fea0fe1adaadeced8c5929efdf37c8c7e8
Aditya-Prakash071 <118518844+Aditya-Prakash071@users.noreply.github.com>
2025-02-13 08:37:26 +0000

    learning first day

---
+++ Learning/file.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+
+
+// sync
+//  fs.writeFileSync('./test.txt','hey aditya quick learn bro ')
+
+//Async
+// fs.writeFile('./text.txt','hey i write with Async',(err)=>console.log(err))
+
+// const demo = fs.readFileSync('./test.txt')
+
+// to encoded  
+
+// const demo = fs.readFileSync('./test.txt','utf-8')
+
+// const demo = fs.readFile('./test.txt','utf-8',(err,result)=>console.log(result))
+
+// to add 
+
+// fs.appendFileSync('./text.txt',`${Date.now()}aditya\n`)
+
+// to copy 
+
+// fs.cpSync('./text.txt','./test.txt')
+
+// to see stat
+
+
+// const demo = fs.statSync('./text.txt').isFile()
+
+// fs.mkdirSync("fol")
+
+// to create a file and go under and create new file 
+
+// fs.mkdirSync('my/a/b',{recursive : true})
+
+
+
+
+
+
+
+// console.log(demo,'demo')
+
+
+
---
+++ Learning/index.js
@@ -0,0 +1,14 @@
+// console.log('hello')
+
+//it is not defined in window 
+// console.log(window);
+
+// related to window objects are is not defined in node js environment 
+// console.log(alert('het'))
+
+
+const math = require('./math')
+
+console.log(math)
+// console.log(math.add(2,7))
+
